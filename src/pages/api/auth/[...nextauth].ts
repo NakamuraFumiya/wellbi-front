e063b2d150ea1a58a291022ff784c619f9918380
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-25 20:53:51 +0900

    tweak

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,10 @@ import GoogleProvider from "next-auth/providers/google";
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SECRET } = process.env
 
+if (!SECRET) throw new Error('You must provide SECRET env var.');
+if (!GOOGLE_CLIENT_ID) throw new Error('You must provide GOOGLE_ID env var.');
+if (!GOOGLE_CLIENT_SECRET) throw new Error('You must provide GOOGLE_SECRET env var.');
+
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
