d8fe4f940ecc40051c4ffcb566a2449be12daa61
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-26 20:06:16 +0900

    見出しが使えるように処理を追加

--- src/components/page/Roadmaps/Plugins/Toolbar/ToolbarPlugin.tsx
+++ src/components/page/Roadmaps/Plugins/Toolbar/ToolbarPlugin.tsx
@@ -1,6 +1,11 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import { TbH1, TbH2, TbH3 } from "react-icons/tb";
 import styles from "./ToolbarPlugin.module.scss";
+import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
+import { HeadingTagType, $createHeadingNode } from "@lexical/rich-text";
+import {$getSelection, $isRangeSelection} from "lexical";
+import { $wrapNodes } from "@lexical/selection";
+
 
 const SupportedBlockType = {
   paragraph: "Paragraph",
@@ -15,6 +20,21 @@ type BlockType = keyof typeof SupportedBlockType;
 
 export const ToolbarPlugin = () => {
   const [blockType, setBlockType] = useState<BlockType>("paragraph");
+  const [editor] = useLexicalComposerContext();
+
+  const formatHeading = useCallback(
+    (type: HeadingTagType) => {
+      if (blockType !== type) {
+        editor.update(() => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            $wrapNodes(selection, () => $createHeadingNode(type));
+          }
+        })
+      }
+    },
+    [blockType, editor]
+  );
 
   return (
     <div className={styles.toolbar}>
@@ -24,6 +44,7 @@ export const ToolbarPlugin = () => {
         title={SupportedBlockType["h1"]}
         aria-label={SupportedBlockType["h1"]}
         aria-checked={blockType === "h1"}
+        onClick={() => formatHeading("h1")}
       >
         <TbH1 className={styles.toolIcon}/>
       </button>
@@ -33,6 +54,7 @@ export const ToolbarPlugin = () => {
         title={SupportedBlockType["h2"]}
         aria-label={SupportedBlockType["h2"]}
         aria-checked={blockType === "h2"}
+        onClick={() => formatHeading("h2")}
       >
         <TbH2 className={styles.toolIcon} />
       </button>
@@ -42,6 +64,7 @@ export const ToolbarPlugin = () => {
         title={SupportedBlockType["h3"]}
         aria-label={SupportedBlockType["h3"]}
         aria-checked={blockType === "h3"}
+        onClick={() => formatHeading("h3")}
       >
         <TbH3 className={styles.toolIcon}/>
       </button>
