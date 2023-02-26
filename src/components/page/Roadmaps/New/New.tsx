f4ba31740279a93653e2e546120059cf0150ed6e
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-26 18:20:25 +0900

    Roadmap作成ページにHeaderを追加

--- src/components/page/Roadmaps/New/New.tsx
+++ src/components/page/Roadmaps/New/New.tsx
@@ -2,6 +2,7 @@ import {Header} from "@/components/ui/Layout/Header/Header";
 import {LexicalComposer} from "@lexical/react/LexicalComposer";
 import {RichTextPlugin} from "@lexical/react/LexicalRichTextPlugin";
 import {ContentEditable} from "@lexical/react/LexicalContentEditable";
+import {useSession} from "next-auth/react";
 
 const initialConfig = {
   namespace: "MyEditor",
@@ -9,15 +10,25 @@ const initialConfig = {
 };
 
 export const New = () => {
+  const { data: session } = useSession();
+
   return (
     <>
       <Header />
-      <LexicalComposer initialConfig={initialConfig}>
-        <RichTextPlugin
-          contentEditable={<ContentEditable />}
-          placeholder={<div>今日もお疲れ様です。執筆されますか？</div>}
-        />
-      </LexicalComposer>
+      {session ? (
+        <>
+          <LexicalComposer initialConfig={initialConfig}>
+            <RichTextPlugin
+              contentEditable={<ContentEditable />}
+              placeholder={<div>今日もお疲れ様です。執筆されますか？</div>}
+            />
+          </LexicalComposer>
+        </>
+      ) : (
+        <>
+          ログインし直してください
+        </>
+      )}
     </>
   )
 };
