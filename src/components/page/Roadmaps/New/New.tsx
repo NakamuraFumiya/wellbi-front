fb354aab6fc1b58e883f1719c47a1149bc54bc0a
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-26 18:23:13 +0900

    tweak

--- src/components/page/Roadmaps/New/New.tsx
+++ src/components/page/Roadmaps/New/New.tsx
@@ -1,13 +1,16 @@
+import { ComponentProps } from "react";
 import {Header} from "@/components/ui/Layout/Header/Header";
 import {LexicalComposer} from "@lexical/react/LexicalComposer";
 import {RichTextPlugin} from "@lexical/react/LexicalRichTextPlugin";
 import {ContentEditable} from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import {useSession} from "next-auth/react";
 
-const initialConfig = {
+const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
   namespace: "MyEditor",
-  onError: (error: Error) => console.error(error),
+  onError: (error) => console.error(error),
 };
+
 
 export const New = () => {
   const { data: session } = useSession();
@@ -21,6 +24,7 @@ export const New = () => {
             <RichTextPlugin
               contentEditable={<ContentEditable />}
               placeholder={<div>今日もお疲れ様です。執筆されますか？</div>}
+              ErrorBoundary={LexicalErrorBoundary}
             />
           </LexicalComposer>
         </>
