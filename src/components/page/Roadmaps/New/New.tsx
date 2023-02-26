114b52ec95059dc2c47df6010d1db2d647fab1a4
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-26 18:12:46 +0900

    lexicalを導入

--- src/components/page/Roadmaps/New/New.tsx
+++ src/components/page/Roadmaps/New/New.tsx
@@ -1,11 +1,23 @@
 import {Header} from "@/components/ui/Layout/Header/Header";
+import {LexicalComposer} from "@lexical/react/LexicalComposer";
+import {RichTextPlugin} from "@lexical/react/LexicalRichTextPlugin";
+import {ContentEditable} from "@lexical/react/LexicalContentEditable";
 
+const initialConfig = {
+  namespace: "MyEditor",
+  onError: (error: Error) => console.error(error),
+};
 
 export const New = () => {
   return (
     <>
-      <Header roadMapNewPage={true} />
-      <h1>Roadmap作成ページです</h1>
+      <Header />
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<div>今日もお疲れ様です。執筆されますか？</div>}
+        />
+      </LexicalComposer>
     </>
   )
 };
