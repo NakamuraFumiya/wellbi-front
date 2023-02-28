48ec163580638e51ba03158ffcf194f3f6b36408
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-28 22:56:04 +0900

    tweak

--- src/components/page/Roadmaps/Plugins/Http/HttpClientPlugin.tsx
+++ src/components/page/Roadmaps/Plugins/Http/HttpClientPlugin.tsx
@@ -1,6 +1,7 @@
 import {useEffect} from "react";
 import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
 import axios from "axios";
+import FormData from "form-data";
 
 type Props = {
   httpRequestHook: boolean,
@@ -14,7 +15,11 @@ export const HttpClientPlugin = ({httpRequestHook}: Props) => {
       const json = editorState.toJSON();
       console.log("投稿する");
       console.log(json.root.children);
-      axios.get("http://localhost:1323/api/posts").then((response) => console.log(response));
+
+      const data = new FormData();
+      data.append("message", json.root.children)
+      axios.post("http://localhost:1323/api/posts", data).then((response) => console.log(response));
+      // axios.get("http://localhost:1323/api/posts").then((response) => console.log(response));
     }
   }, [httpRequestHook])
 
