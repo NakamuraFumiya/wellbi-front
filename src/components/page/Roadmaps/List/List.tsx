6bf4a7a8dbc3cda72b68aad1c46ba58fd9134766
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-03-01 22:31:18 +0900

    tweak

--- src/components/page/Roadmaps/List/List.tsx
+++ src/components/page/Roadmaps/List/List.tsx
@@ -1,8 +1,11 @@
 import {useEffect} from "react";
+import axios from "axios";
+
+const { NEXT_PUBLIC_API_HOST_URL } = process.env
 
 export const List = () => {
   useEffect(() => {
-
+    axios.get(`${NEXT_PUBLIC_API_HOST_URL}/api/posts`).then((response) => console.log(response)).catch((error) => console.log(error))
   }, [])
 
   return (
