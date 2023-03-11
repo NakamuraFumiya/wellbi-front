c2e24d3c1e772ec2637e97740f5adecde5eee131
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-03-11 21:02:07 +0900

    tweak

--- src/components/functional/Cropper/ImageCropper.tsx
+++ src/components/functional/Cropper/ImageCropper.tsx
@@ -1,7 +1,25 @@
 import React, {useState, useCallback, ChangeEvent} from 'react'
-import ReactDOM from 'react-dom'
 import Cropper, {Area} from 'react-easy-crop'
-// import './styles.css'
+import styled from "styled-components";
+
+const StyledCropContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 80px;
+`;
+
+const StyledControls = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  width: 50%;
+  transform: translateX(-50%);
+  height: 40px;
+  display: flex;
+  align-items: center;
+`;
 
 export const ImageCropper = () => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
@@ -12,28 +30,32 @@ export const ImageCropper = () => {
 
   return (
     <>
-      <Cropper
-        image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
-        crop={crop}
-        zoom={zoom}
-        aspect={4 / 3}
-        cropSize={{width: 400, height: 210}}
-        onCropChange={setCrop}
-        onZoomChange={setZoom}
-        onCropComplete={onCropComplete}
-      />
-      <input
-        type="range"
-        value={zoom}
-        min={1}
-        max={3}
-        step={0.1}
-        aria-labelledby="Zoom"
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          setZoom(Number(e.target.value));
-        }}
-        // className="zoom-range"
-      />
+      <StyledCropContainer>
+        <Cropper
+          image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+          crop={crop}
+          zoom={zoom}
+          aspect={4 / 3}
+          cropSize={{width: 400, height: 200}}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          onCropComplete={onCropComplete}
+        />
+      </StyledCropContainer>
+      <StyledControls>
+        <input
+          type="range"
+          value={zoom}
+          min={1}
+          max={3}
+          step={0.1}
+          aria-labelledby="Zoom"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setZoom(Number(e.target.value));
+          }}
+        />
+      </StyledControls>
+
     </>
   )
 };
